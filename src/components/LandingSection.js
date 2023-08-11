3c4e55ae87b9d86a0c2c1d12a12683469ce41c84
SethSchnrt <Seth.Schoenert@gmail.com>
2023-08-10 20:59:04 -0500

    adjusted mobile screen dimensions for landing section

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -3,14 +3,13 @@ import portraitImage from '../assets/IMG_0189.jpeg';
 function LandingSection() {
     return (
         <section className="landing-section">
-            <img src={portraitImage} alt="Portrait of Me" className="portrait"/>
+            <img src={portraitImage} alt="Portrait of Me" className="portrait"/>      
             <div className="overlay-content">
                 <div className="first-name">SETH</div>
                 <div className="last-name">SCHOENERT</div>
                 <p>Fullstack Developer</p>
                 <a className="resumeButton" href="/assets/resume1.pdf" target="_blank" rel="noopener noreferrer">Download My Resume</a>
-            </div>
-            
+            </div>          
         </section>
     );
 }
